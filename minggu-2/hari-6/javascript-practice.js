d6a222f1c77b91a20b38f84a9ea1094e4933323b
fajar241101 <mf.fajarramadhan.sh@gmail.com>
2023-02-21 14:20:45 +0700

    javascript experience

---
+++ minggu-2/hari-6/javascript-practice.js
@@ -0,0 +1,28 @@
+// var tampung = 5;
+// console.log(tampung);
+
+// var angkaGanjil = 1;
+// var angkaGenap = 2;
+// console.log(angkaGanjil);
+// console.log(angkaGenap);
+
+// var tampungBaru;
+// console.log(tampungBaru);
+
+// var nama = "fajar";
+// variabel selain angka harus dikasih string (" ")
+// variabel angka ga dikasih string (6) 
+// console.log(nama);
+
+// let x = 4%2;
+// let y = "4" + 5;
+// console.log(x);
+// console.log(y);
+
+// var bilangan;
+// bilangan = 5;        assignment operator = untuk meng-assign
+// console.log(bilangan);
+
+var angka = "8";
+console.log(angka === 8);
+console.log(angka === 1);
---
+++ minggu-2/hari-6/latihan-18.js
@@ -0,0 +1,33 @@
+// "hello!";
+// document.write("error!");       // tampilan untuk browser
+// console.log("hello, computer!");    // tampilan untuk console
+// alert("hello, human!");         // tampilan untuk browser
+// prompt("What is your name?");   // tampilan untuk browser
+//  console.log("Multiline\n text!");  //tampilan untuk console
+
+// console.log('saya');
+
+// var tampung = 5;
+// console.log(tampung); //5
+// var angkaGanjil = 1;
+// var angkaGenap = 2;
+// var nama = "nama";
+// nama  ='ujang'
+// console.log(nama);
+
+// const apakahPrima = (angka) => {
+//     let pembagi = 0;
+    let number = 100;
+    for(let i=2; i <= number; i++){
+      if(number%i == 0)
+        {
+            console.log(i +'bilangan prima');
+        }  else{
+            console.log(i + 'bilangan bukan prima');
+
+        }    
+
+    
+  }
+
+//   apakahPrima(2);
