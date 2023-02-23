a47cd4f9a6ee8fa5fc9c09aecdf650ab8106bb68
fajar241101 <mf.fajarramadhan.sh@gmail.com>
2023-02-23 15:01:58 +0700

    javascript experience part 2

---
+++ minggu-2/hari-7_kamis-23-2-2023/latihan-19.js
@@ -0,0 +1,134 @@
+// var angka = 8;
+// console.log('<< perandingan sama dengan >>');
+// console.log(angka == 8);
+// console.log(angka == 5);
+// console.log('<< akhir dari perandingan sama dengan >>');
+// console.log('<< != >>');
+// console.log(angka != 8);
+// console.log(angka != 0);
+// var angkaBeda = "1";
+// console.log(angkaBeda === 2);
+// console.log(angkaBeda === 8);
+// console.log(angkaBeda === "1");
+// var angkaBeda = "9";
+// console.log(angkaBeda == 1);
+// console.log(angkaBeda != 3);
+// console.log(angkaBeda === 9);
+// console.log(angkaBeda !== 8);
+// var angka = 100;
+// console.log(angka <= 75);
+// console.log(angka >= 100);
+// console.log(angka < 90);
+// console.log(angka > 100);
+// console.log(true || false);
+// console.log(false || false);
+// console.log(true || true);
+// console.log(false && true && false);
+// console.log(true && true && false);
+// console.log(true && true);
+// var tampung = 7;
+// if(tampung === 5){
+//     console.log("angka yang ditampung adalah 5!");
+// }
+// else{
+//     console.log("angka yang ditampung bukan 5!");
+//     if(tampung != 5){
+//         console.log("angka yang ditampung bukan 5, tapi lebih besar dari 5");
+//     }
+//     else{
+//         console.log("angka yang ditampung bukan 5, tapi lebih kecil dari 5");
+//     }
+// }
+// var tampung = 10;
+// // if(tampung == 5){
+// //     console.log("angka yang ditampung adalah 5");
+// // }
+// // else if(tampung < 5){
+// //     console.log("angka yang ditampung bukan 5, tapi lebih kecil dari 5");
+// // }
+// // else{
+// //     console.log("angka yang ditampung bukan 5, tapi lebih besar dari 5");
+// // }
+
+// tampung == 5 ? console.log("angka yang ditampung adalah 5") : console.log("angka yang ditampung bukan 5"); 
+
+// var buttonPushed = 0;
+// switch (buttonPushed) {
+//     case 1: {console.log('matikan TV'); break;}
+//     case 2: {console.log('turunkan volume'); break;}
+//     case 3: {console.log('naikkan volume'); break;}    
+//     case 4: {console.log('matikan suara TV'); break;}    
+//     default: {console.log('tidak terjadi apa-apa');}
+// }
+
+// var angka = 1;
+// while (angka <= 100) {
+//     console.log('' + angka);
+//     angka++
+// }
+
+// var deret = 5;
+// var jumlah = 1;
+// while (deret > 0) {
+//     jumlah += deret;
+//     deret--;
+//     console.log('jumlah saat ini' + jumlah);
+// }
+// console.log(1umlah);
+
+// for(var angka = 2; angka <= 100; angka++){
+//     console.log('' + angka);
+//     }
+
+// for (let i = 0; i < 10; i+=2) {
+//     console.log('iterasi dengan increment counter 2: ' + i);
+// }
+
+// console.log('------------------');
+// for (let j = 15; j > 0; j-=3) {
+//     console.log('iterasi dengan decrement counter 3: ' + j);
+// }
+
+// var flag = 1;
+// while (flag < 10) {
+//     console.log('iterasi ke-' + flag);
+// }
+
+// let flag =1
+// flag += 1
+// flag = flag + 1
+// flag+2
+// console.log(flag);
+
+// function tampilkan() {
+//     console.log("halo!");
+// }
+// tampilkan();
+
+// function munculkanAngkaDua() {
+//     return 2
+// }
+// var angka = munculkanAngkaDua();
+// console.log(angka);
+
+// function kalikanSepuluh(angka) {
+//     return angka * 10
+// }
+// var tampung = kalikanSepuluh(1);
+// console.log(tampung);
+
+// function tampilkanAngka(angkaPertama, angkaKedua) {
+//     return angkaPertama + angkaKedua
+// }
+// console.log(tampilkanAngka(4,9));
+
+// function tampilkanAngka(angka = 1) {
+//     return angka
+// }
+// console.log(tampilkanAngka(9));
+// console.log(tampilkanAngka());
+
+var fungsiPerkalian = function(angkaPertama, angkaKedua) {
+    return angkaPertama * angkaKedua
+}
+console.log(fungsiPerkalian(3,2));
---
+++ minggu-2/hari-7_kamis-23-2-2023/variables.js
@@ -0,0 +1,50 @@
+'<< Create a Variable: var >>'
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+'<< Create a Variable: let >>'
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+'<< Create a Variable: const >>'
+const entree = 'Enchiladas';
+console.log(entree);
+'<< Mathematical Assignment Operators >>'
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+
+
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp += 5); 
+console.log('The value of powerLevel:', powerLevel -= 100); 
+console.log('The value of multiplyMe:', multiplyMe *= 11); 
+console.log('The value of quarterMe:', quarterMe /= 4);
+'<< The Increment and Decrement Operator >>'
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+console.log(gainedDollar);
+lostDollar--;
+console.log(lostDollar);
+'<< String Concatenation with Variables >>'
+let favoriteAnimal = 'cat';
+console.log('My favorite animal: ' + favoriteAnimal);
+'<< String Interpolation >>'
+var myName = 'Fajar';
+let myCity = 'Manchester';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+'<< typeof operator >>'
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
